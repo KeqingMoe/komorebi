33c095ced4d14b6779a1924d4fc913561cc0b73e
Teru Shigure <me@keqing.moe>
2026-02-24 20:06:47 +0800

    refactor: extract sorting

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -11,6 +11,22 @@ export async function getPosts<E extends CollectionEntry<keyof DataEntryMap>>(
   });
 }
 
+export async function getSortedPosts<E extends CollectionEntry<keyof DataEntryMap>>(
+  filter?: (entry: CollectionEntry<keyof DataEntryMap>) => entry is E,
+) {
+  const posts = await getPosts(filter);
+  sortByPubDate(posts);
+  return posts;
+}
+
+export function sortByPubDate(posts: CollectionEntry<'blog'>[]) {
+  posts.sort((a, b) => {
+    let ta = a.data.pubDate.valueOf();
+    let tb = b.data.pubDate.valueOf();
+    return ta - tb;
+  });
+}
+
 export function getPostUrl(id: string) {
   return `/blog/${id}`;
 }
