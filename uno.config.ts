32f6548aab4f17198fe831866dd7958a96849146
Teru Shigure <me@keqing.moe>
2026-02-25 22:19:19 +0800

    feat: add pagination navigator styling

--- uno.config.ts
+++ uno.config.ts
@@ -2,7 +2,8 @@ import {
   defineConfig,
   presetAttributify,
   presetTypography,
-  presetWind4
+  presetWind4,
+  transformerDirectives
 } from 'unocss';
 
 export default defineConfig({
@@ -10,5 +11,18 @@ export default defineConfig({
     presetWind4(),
     presetAttributify(),
     presetTypography(),
-  ]
+  ],
+  transformers: [
+    transformerDirectives(),
+  ],
+  theme: {
+    colors: {
+      base: {
+        100: 'oklch(100% 0 0)',
+        200: 'oklch(98% 0 0)',
+        300: 'oklch(95% 0 0)',
+        content: 'oklch(21% .006 285.885)',
+      },
+    }
+  },
 });
