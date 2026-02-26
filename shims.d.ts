17296f49dd4f76050394fab9bbde65faf0644126
Teru Shigure <me@keqing.moe>
2026-02-26 09:01:12 +0800

    feat: add header styling

--- shims.d.ts
+++ shims.d.ts
@@ -1,7 +1,14 @@
 import type { AttributifyAttributes } from '@unocss/preset-attributify';
 
+type VariantPrefixes = 'print';
+type UtilityNames = 'hidden' | 'block';
+
+type Variants = {
+  [K in VariantPrefixes | `${VariantPrefixes}:${UtilityNames}`]?: boolean | string;
+};
+
 declare global {
   namespace astroHTML.JSX {
-    interface HTMLAttributes extends AttributifyAttributes { }
+    interface HTMLAttributes extends AttributifyAttributes, Variants { }
   }
 }
